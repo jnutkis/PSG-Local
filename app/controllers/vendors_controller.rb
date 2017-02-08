674bb9d9639944c72df27089d1b385b8c279b2ad
James <james.nutkis@gmail.com>
2017-02-07 20:53:22 -0600

    Vendors index

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,7 +1,9 @@
 class VendorsController < ApplicationController
   
   def index
-
+    @vendors = Vendor.all.order(:name).page(params[:page]).per(25)
+    @q = Vendor.ransack(params[:q])
+    @r = @q.result(distinct:true).order(:name).page(params[:page]).per(25)
   end
   
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,5 +2,5 @@ class Vendor < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name
   validates :name, presence: true
-  validated :active, presence: true
+  validates :active, presence: true
 end
