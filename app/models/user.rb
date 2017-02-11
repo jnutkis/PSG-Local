c3c143a1b4f50b54002a893e1791794dfb85e77e
James <james.nutkis@gmail.com>
2017-02-11 16:15:20 -0600

    User Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  
+  validates :firstname, presence:true, length: {maximum:30}
+  validates :lastname, presence:true, length: {maximum:30}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 50},format: {with: VALID_EMAIL_REGEX}
 end
