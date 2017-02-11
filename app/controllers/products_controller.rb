749a24933be6d30c890c87abbf978e92d5e7282b
James <james.nutkis@gmail.com>
2017-02-10 21:57:08 -0600

    Product Page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,5 @@
 class ProductsController < ApplicationController
+  def show
+    @product = Product.friendly.find(params[:id])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :vendor
   has_many :associations
   has_many :references, :through => :associations
+  extend FriendlyId
+  friendly_id :id
   validates :name, presence: true
 end
