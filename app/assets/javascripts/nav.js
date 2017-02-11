3df362c1b26b936a45fdd5d4b61abea2467edbbf
James <james.nutkis@gmail.com>
2017-02-10 19:46:18 -0600

    Nav bar

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,1 +1,12 @@
+ $("#close_menu").click(function() {
+        $("#sidebar-wrapper").css("width",0);
+        $("#wrapper").css("paddingLeft",0);
+        $("#open_menu").css({"visibility": "visible","z-index" : 2}).animate({opacity: 0},1).animate({opacity: 1},1000);
+ });
 
+
+ $("#open_menu").click(function() {
+        $("#sidebar-wrapper").css("width",250);
+        $("#wrapper").css("paddingLeft", 250);
+        $("#open_menu").css("visibility","hidden");
+ });
