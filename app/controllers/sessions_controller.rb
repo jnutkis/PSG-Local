544d47f07ade697fb1915344d60a349c2f85013d
James <james.nutkis@gmail.com>
2017-02-12 14:18:49 -0600

    User Login Working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    
+    if user && user.authenticate(params[:session][:password])
     redirect_to root_path
+    else
+      render 'new'
+    end
+  
   end
   
   def destroy
