8ef0bf49a4190d5e5a3b09748058929867038ef0
James <james.nutkis@gmail.com>
2017-02-11 21:10:00 -0600

    Did we fix it?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211213457) do
+ActiveRecord::Schema.define(version: 20170212030856) do
 
   create_table "associations", force: :cascade do |t|
     t.integer "reference_id"
