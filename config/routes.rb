2c2880dba4562c5381a07c14938fec48b0c8ee01
James <james.nutkis@gmail.com>
2017-02-12 13:32:26 -0600

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get '/login/',to: 'sessions#new'
-  
+  post 'login',to: 'sessions#create'
+  delete '/logout/', to: 'sessions#destroy'
   
   
   resources :references
