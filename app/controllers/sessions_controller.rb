b490be5d45b3968bd7a508c736a50d92fe252abf
James <james.nutkis@gmail.com>
2017-02-12 14:34:46 -0600

    Auth sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     
     if user && user.authenticate(params[:session][:password])
-    redirect_to root_path
+      log_in user
+      redirect_to root_path
     else
+      flash.now[:danger] = "Invalid Username and Password"
       render 'new'
     end
   
