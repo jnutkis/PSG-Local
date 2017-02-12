e89ffeeeec7504a2c33824b825892267bfbebf94
James <james.nutkis@gmail.com>
2017-02-12 11:43:03 -0600

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   validates :firstname, presence:true, length: {maximum:55}
   validates :lastname, presence:true, length: {maximum:55}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_secure_password
 end
