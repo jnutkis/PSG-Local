bf0eb9cd064dafe42a7fd101a833e80d2cce3eaa
James <james.nutkis@gmail.com>
2017-02-12 13:14:07 -0600

    User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+  get '/login/',to: 'users#login'
+  
+  
   
   resources :references
   resources :vendors do
     resources :products
   end
+  
+  
 end
