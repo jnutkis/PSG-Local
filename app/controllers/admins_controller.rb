ca76df02012a7fe5c9a95e1d53464d78b9cbabb4
James <james.nutkis@gmail.com>
2017-02-13 07:05:35 -0600

    Edit

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,11 +2,11 @@ class AdminsController < ApplicationController
   def index
     if !admin? && !super?
       redirect_to root_path
-    end
-    
+    else
     @vendors = Vendor.all.order(:name)
     @references = Reference.all.order(:full_ref)
     
     @vendor = Vendor.find_by(id: current_user.vendor_id)
+   end
   end
 end
