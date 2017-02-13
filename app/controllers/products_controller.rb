1f1a2fb69a5ced3d9afff5145ddd400c5e37b919
James <james.nutkis@gmail.com>
2017-02-12 18:33:24 -0600

    create-product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,31 @@ class ProductsController < ApplicationController
     @vendor = Vendor.friendly.find(params[:vendor_id])
     @product = @vendor.products.friendly.find(params[:id])
   end
+  def new
+    @product = Product.new
+    @vendor = Vendor.find_by(id: current_user.vendor_id).name
+    if !@vendor == params[:vendor_id]
+      redirect_to root_path
+    end
+  end
+  
+  def create
+    @vendor = Vendor.find_by(id: current_user.vendor_id).name
+    if @vendor == params[:vendor_id]
+      @product = Product.new(product_params)
+      @product.update(vendor: Vendor.find_by(id: current_user.vendor_id))
+      if @product.save
+        redirect_to administration_path
+      end
+      
+    else
+      redirect_to root_path
+    end
+  end
+  
+    private
+    def product_params
+      params.require(:product).permit(:name,:description,:url,:references)
+    end
+
 end
