b21548cf3463d1ed761920d0ff1e9d1530d76033
James <james.nutkis@gmail.com>
2017-02-14 18:01:05 -0600

    User Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def login
     
   end
+  
+  def index
+    @user = current_user
+    if !logged_in? || current_user != User.find(params[:id])
+      redirect_to root_path
+    end
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+
+  end
+  
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'login',to: 'sessions#create'
   delete '/logout/', to: 'sessions#destroy'
   get '/administration/', to: "admins#index"
+  get '/user/:id', to: 'users#index', as: 'user'
   
   
   resources :references
