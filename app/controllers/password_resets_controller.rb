cdc2ebba17da8858a359d70c2f664a0b89deac96
James <james.nutkis@gmail.com>
2017-02-18 19:53:29 -0600

    Reset start

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,22 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:success] = "If exists, email sent with password reset instructions"
+      redirect_to login_path
+    else
+      flash[:danger] = "If exists, email sent with password reset instructions"
+      redirect_to login_path
+    end
+  end
+  
+  
+  
   def edit
+    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :activation_token, :temporary_token
+  attr_accessor :activation_token, :temporary_token, :reset_token
   attr_readonly :email
   before_create :create_activation_digest
   before_validation :create_temp_digest, on: :create 
@@ -46,7 +46,17 @@ class User < ActiveRecord::Base
     UserMailer.account_activation(self).deliver_now
   end
 
-  
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
   
   
   
