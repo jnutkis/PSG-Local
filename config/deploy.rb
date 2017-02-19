0cb18864d0cf8ff7c8e973df2a2543a4e43d4c79
James <james.nutkis@gmail.com>
2017-02-19 16:02:37 -0600

    Hope i didnt screw this up

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "3.7.2"
 
 set :application, "PSG"
-set :repo_url, "git@github.com:jnutkis/PSG-Local.git"
+set :repo_url, "https://github.com/jnutkis/PSG-Local.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "'/home/deploy/PSG"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -31,3 +31,6 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :scm, :git
+set :branch, "master"
