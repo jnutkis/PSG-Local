3558186434f838dfb40977ce027ebb693dee7de1
James Nutkis <james.nutkis@gmail.com>
2017-02-27 16:14:09 +0000

    Refs

--- app/assets/javascripts/reference.js
+++ app/assets/javascripts/reference.js
@@ -9,8 +9,8 @@ $( document ).ready(function () {
 $( document ).ready(function () {
     $('#reference-table-results').DataTable({
     aLengthMenu: [
-        [15,50, 100, -1],
-        [15,50, 100, "All"]],
+        [20,50, 100, -1],
+        [20,50, 100, "All"]],
         "searching": false,
         "pagingType": "full_numbers"
     
