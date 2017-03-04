0c83cba98a9bf347822c2f1463355f4d33dffb4d
James Nutkis <james.nutkis@gmail.com>
2017-03-04 17:38:09 +0000

    Vendor

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -63,24 +63,3 @@ $( document ).ready(function () {
     }
 });
 
-
-
-var vendorSizing = function() {
-     var width = $(".vendor-list").width();
-     if (width) {
-         var $list = $(".vendor-list-item");
-         if (width > 912) {
-             $list.css({
-                 width: (width-12)/3
-             })
-         } else if (width > 480) {
-             $list.css({
-                 width: (width-8)/2
-             })             
-         } else {
-             $list.css({
-                 width: (width-4)
-             })              
-         }
-     }
-}
