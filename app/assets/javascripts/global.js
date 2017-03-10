776f8773b0c57654f32b2b4ccb07465ab393bae0
James Nutkis <james.nutkis@gmail.com>
2017-03-10 17:10:20 +0000

    User profile JS

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -14,8 +14,19 @@ var searchTile = function() {
 
 
 var profileTitleSize = function() {
-    var width = $(".profile-wrapper form").outerWidth();
-    $(".profile-wrapper h1").css("left",((width/2)-75));
+    if ($(".profile-wrapper-other h1").html() ==  null) {
+        var width = $(".profile-wrapper form").outerWidth();
+        $(".profile-wrapper h1").css("left",((width/2)-75));
+    } else {
+        var width = $(".profile-wrapper-other form").outerWidth();
+        var len = $(".profile-wrapper-other h1").html().length;
+        if ((width/2)-(len*5) > 0) {
+            $(".profile-wrapper-other h1").css("left",((width/2)-(len*5)));
+        } else {
+            $(".profile-wrapper-other h1").css("left",0); 
+        }
+    }
+    
 };
 
 
