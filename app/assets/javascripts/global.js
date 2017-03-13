a63ae0094ef177c0b11e74c9431d9c135c7212bf
James Nutkis <james.nutkis@gmail.com>
2017-03-13 00:09:16 +0000

    js for ref

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -40,20 +40,76 @@ var profileTitleSize = function() {
     $("input#product_reference_ids").next().trigger("click");
 };*/
 
+var refCount 
+var aPage = 0
+var bPage = 25;
+
+
+var pageNext = function(a,b){
+    var next = $(".ref-form #ref-next");
+    var back = $(".ref-form #ref-back");
+    
+     if (a < (refCount-50)) { /*if a param is more than 25 of the count*/
+        aPage += 25;
+        bPage += 25;
+        back.show();
+        return referencePagination(aPage,bPage);
+     } else {
+        aPage += 25;
+        bPage = refCount;
+        next.hide();
+        return referencePagination(aPage,bPage);
+     }
+};
+
+var pageBack = function(a,b){
+    var next = $(".ref-form #ref-next");
+    var back = $(".ref-form #ref-back");
+    
+     if (b > 50) { /*if a param is more than 25 of the count*/
+        aPage -= 25;
+        bPage -= 25;
+        next.show();
+        return referencePagination(aPage,bPage);
+     } else {
+        aPage = 0;
+        bPage -= 25;
+        back.hide();
+        return referencePagination(aPage,bPage);
+     }
+};
+
+var pageAll = function(){
+    var next = $(".ref-form #ref-next");
+    var back = $(".ref-form #ref-back");
+    
+    referencePagination(0,refCount);
+    next.hide();
+    back.hide();
+    $(".ref-form #ref-all").hide();
+}
+
 
 var referencePagination = function (a,b) {
-  var div,a,b;
-  
- a = (typeof a !== 'undefined') ?  a : 0
- b = (typeof b !== 'undefined') ?  b : 24
+ 
+ if (a == null) {
+     a = 0;
+ }
+ 
+ if (b == null) {
+     b = 25;
+ }
   
  div = $(".ref-form .checkbox");
-
+ count = $(".ref-form .checkbox").length;
+ refCount = count;
+ 
  div.hide();
  div.slice(a,b).show();
   
- return [a,b];  
 
+ console.log([a,b])
+ 
 };
 
 
@@ -64,6 +120,19 @@ $(document).ready(function(){
     searchTile();
     profileTitleSize();
     referencePagination();
+    
+    $(".ref-form #ref-next").click(function(){
+        pageNext(aPage,bPage);
+    });
+    
+    $(".ref-form #ref-back").click(function(){
+        pageBack(aPage,bPage);
+    });
+    
+    $(".ref-form #ref-all").click(function(){
+        pageAll();
+    })
+    
 });
 
 $(window).resize(function(){
